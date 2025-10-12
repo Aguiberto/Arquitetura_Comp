ce017f624838581b464c30d133638d57538e3c93
Aguiberto <aguibertocandido@gmail.com>
2025-10-11 21:20:28 -0300

    Q16

--- Lista_02/16_CPF.asm
+++ Lista_02/16_CPF.asm
@@ -1,217 +1,160 @@
 .text
 	# esse programa recebe 9 números e apresenta mais 2 formando um cpf válido
 main:
+
+#--------- ENTRADAS ------------------------
+
 	addi $2,$0,5
-	syscall			# entrada 1
+	syscall			# 1
 	add $8,$0,$2
 	
-	addi $9,$0,10
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$0,$10		# $11 acumulador
+	addi $2,$0,5
+	syscall			# 2
+	add $9,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 2
-	add $8,$0,$2
-	
-	addi $9,$0,9
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 3
+	add $10,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 3
-	add $8,$0,$2
-	
-	addi $9,$0,8
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 4
+	add $11,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 4
-	add $8,$0,$2
-	
-	addi $9,$0,7
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 5
+	add $12,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 5
-	add $8,$0,$2
-	
-	addi $9,$0,6
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 6
+	add $13,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 6
-	add $8,$0,$2
-	
-	addi $9,$0,5
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 7
+	add $14,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 7
-	add $8,$0,$2
-	
-	addi $9,$0,4
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
+	syscall			# 8
+	add $15,$0,$2
 	
 	addi $2,$0,5
-	syscall			# entrada 8
-	add $8,$0,$2
+	syscall			# 9
+	add $16,$0,$2
 	
-	addi $9,$0,3
-	mul $10,$9,$8		# primeira multiplicação
 	
-	add $11,$11,$10		# $11 acumulador
+# --------------- PRIMEIRO DIGITO --------------------
+
+	mul $17,$8,10
+	add $18,$0,$17
 	
-	addi $2,$0,5
-	syscall			# entrada 9
-	add $8,$0,$2
+	mul $17,$9,9
+	add $18,$18,$17
 	
-	addi $9,$0,2
-	mul $10,$9,$8		# primeira multiplicação
+	mul $17,$10,8
+	add $18,$18,$17
 	
-	add $11,$11,$10		# SOMA TOTAL
+	mul $17,$11,7
+	add $18,$18,$17
 	
-	addi $12,$0,11
-	div $11,$12		
-	mfhi $13		# resto da divisão por 11
+	mul $17,$12,6
+	add $18,$18,$17
 	
-	sub $8,$13,$12		# PRIMEIRO DIGITO = $8
-	addi $9,$0,10
-	slt $14,$8,$9
-	beq $14,$0,PrimeiroZero
+	mul $17,$13,5
+	add $18,$18,$17
 	
-	add $4,$0,$8		# PRIEMEIRO DIGITO, quando for não for 0
-	addi $2,$0,5
+	mul $17,$14,4
+	add $18,$18,$17
+	
+	mul $17,$15,3
+	add $18,$18,$17
+	
+	mul $17,$16,2
+	add $18,$18,$17		# SOMA TOTAL
+	
+	mul $18,$18,10
+	addi $17,$0,11
+	div $18,$17
+	mfhi $19		# PRIMEIRO DIGITO OPÇÃO 1
+	
+	addi $17,$0,10
+	beq $19,$17,DigitoZero
+	
+	add $4,$0,$19
+	addi $2,$0,1
+	syscall
+	
+	addi $4,$0,'\n'
+	addi $2,$0,11
 	syscall
 	
 	j SegundoDigito
 	
-PrimeiroZero:
-	add $4,$0,$0
-	addi $2,$0,5
+DigitoZero:
+	
+	addi $4,$0,0		# PRIMEIRO DIGITO OPÇÃO 2
+	addi $2,$0,1
+	syscall
+
+	addi $4,$0,'\n'
+	addi $2,$0,11
 	syscall
 	
-	
+# ------------- SEGUNDO DIGITO -----------------------------
+
 SegundoDigito:
-	addi $2,$0,5
-	syscall			# entrada 1
-	add $8,$0,$2
 	
-	addi $9,$0,11
-	mul $10,$9,$8		# primeira multiplicação
+	addi $18,$0,0
 	
-	add $11,$0,$10		# $11 acumulador
+	mul $17,$8,11
+	add $18,$0,$17
 	
-	addi $2,$0,5
-	syscall			# entrada 2
-	add $8,$0,$2
+	mul $17,$9,10
+	add $18,$18,$17
 	
-	addi $9,$0,10
-	mul $10,$9,$8		# primeira multiplicação
+	mul $17,$10,9
+	add $18,$18,$17
 	
-	add $11,$11,$10		# $11 acumulador
+	mul $17,$11,8
+	add $18,$18,$17
 	
-	addi $2,$0,5
-	syscall			# entrada 3
-	add $8,$0,$2
+	mul $17,$12,7
+	add $18,$18,$17
 	
-	addi $9,$0,9
-	mul $10,$9,$8		# primeira multiplicação
+	mul $17,$13,6
+	add $18,$18,$17
 	
-	add $11,$11,$10		# $11 acumulador
+	mul $17,$14,5
+	add $18,$18,$17
 	
-	addi $2,$0,5
-	syscall			# entrada 4
-	add $8,$0,$2
+	mul $17,$15,4
+	add $18,$18,$17
 	
-	addi $9,$0,8
-	mul $10,$9,$8		# primeira multiplicação
+	mul $17,$16,3
+	add $18,$18,$17
 	
-	add $11,$11,$10		# $11 acumulador
+	mul $17,$19,2
+	add $18,$18,$17		# SOMA TOTAL	
 	
-	addi $2,$0,5
-	syscall			# entrada 5
-	add $8,$0,$2
+	mul $18,$18,10
+	addi $17,$0,11
+	div $18,$17
+	mfhi $18		# SEGUNDO DIGITO OPÇÃO 1
 	
-	addi $9,$0,7
-	mul $10,$9,$8		# primeira multiplicação
+	addi $17,$0,10
+	beq $18,$17,DigitoZeros
 	
-	add $11,$11,$10		# $11 acumulador
-	
-	addi $2,$0,5
-	syscall			# entrada 6
-	add $8,$0,$2
-	
-	addi $9,$0,6
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
-	
-	addi $2,$0,5
-	syscall			# entrada 7
-	add $8,$0,$2
-	
-	addi $9,$0,5
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
-	
-	addi $2,$0,5
-	syscall			# entrada 8
-	add $8,$0,$2
-	
-	addi $9,$0,4
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# $11 acumulador
-	
-	addi $2,$0,5
-	syscall			# entrada 9
-	add $8,$0,$2
-	
-	addi $9,$0,3
-	mul $10,$9,$8		# primeira multiplicação
-	
-	addi $2,$0,5
-	syscall			# entrada 9
-	add $8,$0,$2
-	
-	addi $9,$0,2
-	mul $10,$9,$8		# primeira multiplicação
-	
-	add $11,$11,$10		# SOMA TOTAL
-	
-	addi $12,$0,11
-	div $11,$12
-	mfhi $13		# resto da divisão
-	sub $14,$12,$13	
-	
-	addi $8,$0,10
-	slt $15,$14,$8
-	beq $15,$0,SegundoZero
-	
-	add $4,$0,$14
+	add $4,$0,$18
 	addi $2,$0,1
 	syscall
 	
 	j fim
+
+DigitoZeros:
 	
-SegundoZero:
-	add $4,$0,$0
+	addi $4,$0,0
 	addi $2,$0,1
 	syscall
-
+	
 fim:
 	addi $2,$0,10
-	syscall
+	syscall	
+	
