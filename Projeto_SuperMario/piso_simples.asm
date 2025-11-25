698433c81917713548111c91ad366a25f87ea4ca
unknown <aguibertocandido@gmail.com>
2025-11-25 13:32:28 -0300

    3 elementos

---
+++ Projeto_SuperMario/piso_simples.asm
@@ -0,0 +1,18 @@
+.text
+
+main: 	lui $8, 0x1001
+	ori $9, 0xbf3e0f
+	
+	addi $10,$10,1280	# o piso ocupa 1280 endereços de memória
+	
+piso:	beq $10,$0,fim
+
+	sw $9,27648($8)
+	addi $8,$8,4
+	addi $10,$10,-1
+	
+	j piso
+	
+fim:	addi $2,$0,10
+	syscall
+	
